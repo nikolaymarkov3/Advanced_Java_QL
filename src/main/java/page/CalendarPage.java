87e6a239e86480106ac01bd520a39792bedcf3fe
Nikolai Markow <i@nikomar.ru>
2025-02-05 15:57:41 +0700

    22коммит

--- src/main/java/page/CalendarPage.java
+++ src/main/java/page/CalendarPage.java
@@ -13,10 +13,17 @@ import java.util.Objects;
 
 @Getter
 public class CalendarPage extends BasePage{
+//    public CalendarPage(WebDriver driver) {
+//        super(driver);
+//    }
+
+    private final WebDriver driver;
+
     public CalendarPage(WebDriver driver) {
         super(driver);
+        this.driver = driver;
     }
-    
+
     @FindBy(xpath = "//*[@href='/calendar/']")
     private WebElement openCalendar;
     
