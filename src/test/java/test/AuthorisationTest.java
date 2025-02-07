c92ff9bd50e4281fd9ba24ac15c5de48a690ac94
Nikolai Markow <i@nikomar.ru>
2025-02-07 15:14:00 +0700

    25коммит

--- src/test/java/test/AuthorisationTest.java
+++ src/test/java/test/AuthorisationTest.java
@@ -44,12 +44,12 @@ public class AuthorisationTest extends BaseTest{
 	@Description("Проверка авторизации")
 	void testBv() {
 		String expectResult = "Вы хотите залогировать больше или меньше 5 часов, которые по графику запланированы у вас на сегодня";
-		calendarPage.openCalendar(driver);
-//				.moveReportsAndClickTodayReport(driver);
-//
-//		String actualResult = todayReportPage.clickMode()
-//				.meaningModalLabel();
-//		assertEquals(actualResult, expectResult);
+		calendarPage.openCalendar(driver)
+				.moveReportsAndClickTodayReport(driver);
+
+		String actualResult = todayReportPage.clickMode()
+				.meaningModalLabel();
+		assertEquals(actualResult, expectResult);
 	}
 
 	@Test
