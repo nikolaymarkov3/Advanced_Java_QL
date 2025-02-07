9a08615b18b228495fa867d6d951d9f00f6c3ba9
Nikolai Markow <i@nikomar.ru>
2025-02-07 18:17:20 +0700

    25коммит

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -37,8 +37,9 @@ public class BaseTest {
 		if (driver == null) {
 //			System.setProperty("webdriver.chrome.driver", "N:/DESCTOP/Chromedriver/chromedriver-win64/chromedriver");
 //			driver = new ChromeDriver(options);
-//			driver = new RemoteWebDriver(new URL("http://selenium-hub1:4444/wd/hub"), options);
-			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+//			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+			driver = new RemoteWebDriver(new URL("http://selenium-hub1:4444/wd/hub"), options);
+//			driver = new RemoteWebDriver(new URL("http://selenoid:4444/wd/hub"), options);
 			Logger.logInfo("Инициализация драйвера");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 			driver.manage().window().maximize();
